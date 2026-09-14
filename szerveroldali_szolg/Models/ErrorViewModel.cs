2de0c66f765a1e53b1d408fd2cb8f0867d3a5062
WyLToR <bekesi.patrik@gmail.com>
2026-09-14 10:49:58 +0200

    Add project files.

---
+++ szerveroldali_szolg/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace szerveroldali_szolg.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
