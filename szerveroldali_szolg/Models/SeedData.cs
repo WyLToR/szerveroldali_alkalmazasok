9a82b58a183f88eb0fec4e0cca4fbcf98243b4fe
WyLToR <bekesi.patrik@gmail.com>
2026-09-17 12:42:31 +0200

    feat: add a new field

--- szerveroldali_szolg/Models/SeedData.cs
+++ szerveroldali_szolg/Models/SeedData.cs
@@ -21,6 +21,7 @@ namespace szerveroldali_szolg.Models
                         Title = "When Harry Met Sally",
                         ReleaseDate = DateTime.Parse("1989-2-12"),
                         Genre = "Romantic Comedy",
+                        Rating = "R",
                         Price = 7.99M
                     },
                     new Movie
